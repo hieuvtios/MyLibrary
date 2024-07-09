06602d1897258ddd437eff93641b28a9b8dbc345
hieuvtios <hieuvt@infinitytech.global>
2024-07-09 10:39:07 +0700

    1
    1

---
+++ Sources/MyLibrary/SDK.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hieu Vu on 9/7/24.
+//
+
+import Foundation
+public struct SDK{
+    public static func doSomeWork(){
+        print("Doing some work...")
+    }
+}
